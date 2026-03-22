cb83d73a74317c1d431bc89ac8f238c36f18b21b
dongfengtaoadmin <dongfengtao@authing.com>
2026-03-22 18:56:22 +0800

    Initial commit

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+// api请求接口前缀
+export const apiPrefix: string = 'http://localhost:9000'
+
+// 业务桩体阿妈
+export const httpCode = {
+    success: 'success',
+    fail: 'fail',
+    notFound: 'not_found',
+    unauthorized: 'unauthorized',
+    forbidden: 'forbidden',
+    validateError: 'validate_error',
+  }
+  
---
+++ src/utils/auth.ts
@@ -0,0 +1,6 @@
+// 检测是否登录
+export const isLogin = () => {
+  // todo:授权认证逻辑待实现
+  
+  return true
+}
